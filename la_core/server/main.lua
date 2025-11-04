470ed1e1ba9cd0c36e80ef6975ea6f12d9178c08
Martin Dunn <martindunn82@gmail.com>
2025-11-04 15:53:03 -0500

    Refactor la_status command and logging mechanism

--- la_core/server/main.lua
+++ la_core/server/main.lua
@@ -1,13 +1,33 @@
-RegisterCommand("la_status", function(source)
-    local src = source
-    local msg = "[la_core] Active=true"
-    if src == 0 then
-        print(msg)
-    else
-        TriggerClientEvent("chat:addMessage", src, { args = { msg } })
-    end
-end, false)
+local Config = require("config")
 
-CreateThread(function()
-    print("[la_core] v1.0.2 loaded on server.")
-end)
+local Core = {}
+
+local function emitLog(level, message)
+    local msg = string.format("[la_core][%s] %s", level, message)
+    print(msg)
+    return msg
+end
+
+function Core.init(opts)
+    if type(opts) == "table" then
+        for key, value in pairs(opts) do
+            Config[key] = value
+        end
+    end
+
+    RegisterCommand("la_status", function(source)
+        local src = source
+        local msg = emitLog("info", "Active=true")
+        if src ~= 0 then
+            TriggerClientEvent("chat:addMessage", src, { args = { msg } })
+        end
+    end, false)
+
+    CreateThread(function()
+        emitLog("info", "v1.0.2 loaded on server.")
+    end)
+
+    return { ok = true }
+end
+
+return Core
