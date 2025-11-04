6c64fdf8ab05aac9fe6173f1697c139bed5eec51
Martin Dunn <martindunn82@gmail.com>
2025-11-04 15:53:45 -0500

    Refactor main.lua to include CoreClient initialization
    Refactor la_core client to initialize with options and maintain status command.

--- la_core/client/main.lua
+++ la_core/client/main.lua
@@ -1,9 +1,19 @@
--- client/main.lua inside la_core or your custom resource
-local isAppearanceOpen = false
+local Config = require("config")
 
--- /la_status client-side test
-RegisterCommand("la_status", function()
-    print("[la_core] Active=true")
-end, false)
+local CoreClient = {}
 
+function CoreClient.init(opts)
+    if type(opts) == "table" then
+        for key, value in pairs(opts) do
+            Config[key] = value
+        end
+    end
 
+    RegisterCommand("la_status", function()
+        print("[la_core] Active=true")
+    end, false)
+
+    return { ok = true }
+end
+
+return CoreClient
